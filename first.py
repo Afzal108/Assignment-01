c06ffc1c9ca053888e8fba276f82bb3327d9298a
Afzal108 <afzalkbas9900@gmail.com>
2024-11-02 22:48:44 +0500

    first.py

---
+++ first.py
@@ -0,0 +1,70 @@
+#Question 01
+#sum
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+num3=int(input("Enter number 3"))
+sum=num1+num2+num3
+print(num1,num2,num3)
+print(sum)
+
+#Difference
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+num3=int(input("Enter number 3"))
+difference=num1-num2-num3
+print(num1,num2,num3)
+print(difference)
+
+#product
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+num3=int(input("Enter number 3"))
+product=num1*num2*num3
+print(num1,num2,num3)
+print(product)
+
+#Average
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+num3=int(input("Enter number 3"))
+average=num1+num2+num3/3
+print(num1,num2,num3)
+print(average)
+                #done first question and start second question.
+#Area of triangle
+base=int(input("Enter base of triangle"))
+height=int(input("Enter height of triangle"))
+area=0.5*base*height
+print(area)
+
+#Area of square
+side=int(input("Enter side of square"))
+area=side*side
+print(area)
+
+#Area of circle 
+radius=int(input("Enter radius of circle"))
+area=3.14*radius**2
+print(area)
+
+#Area of Rectangle 
+Length=int(input("Enter length of rectangle"))
+Width=int(input("Enter width of rectangle"))
+area=Length*Width
+print(area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
